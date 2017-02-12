2d8e52c3ed85f5c3651fdbe49e7840aab54c21d7
Mark <youngce0918@gmail.com>
2017-02-12 22:08:08 +0800

    init

---
+++ solutions/pow.go
@@ -0,0 +1,18 @@
+package solutions
+//https://leetcode.com/problems/powx-n/
+func myPow(x float64, n int) float64 {
+
+	if n==0{
+		return 1
+	}
+	if n<0{
+		x=1/x
+		n=-n
+	}
+	if n%2==0 {
+		return myPow(x*x,n/2)
+	}
+	return x*myPow(x*x,n/2)
+
+
+}
