80e5a021796436dbf6ef30eab72e11aab7bf3caf
Mark <youngce0918@gmail.com>
2017-10-06 12:10:22 +0800

    #14 Longest Common Prefix

---
+++ solutions/LongestCommonPrefix.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getCommonPrefix(s1 string, s2 string) string {
+	if !strings.HasPrefix(s2,s1){
+		return getCommonPrefix(s2,s1[:len(s1)-1])
+	} else {
+		return s1
+	}
+}
+func longestCommonPrefix(strs []string) string {
+
+	var prefix string
+
+
+	size:=len(strs)
+	if size>0{
+		prefix=strs[0]
+		tails:=strs[1:]
+		fmt.Println(tails)
+		for _,str:=range tails {
+			prefix=getCommonPrefix(prefix,str)
+			if prefix==""{
+				break
+			}
+
+		}
+	}
+
+	//for _,str:=range strs{
+	//	length:=len(str)
+	//	fmt.Println(length)
+	//	if max<len(str){
+	//		max=length
+	//		prefix =str
+	//	}
+	//
+	//}
+	return prefix
+}
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"abc"}))
+}
