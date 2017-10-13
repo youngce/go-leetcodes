f702d26636259bfdd791d29fdcab7bd4e04ade57
Mark <youngce0918@gmail.com>
2017-10-13 16:21:52 +0800

    #83. Remove Duplicates from Sorted List Done!!

---
+++ solutions/RemoveDuplicatesFromSortedList.go
@@ -0,0 +1,15 @@
+package solutions
+
+func deleteDuplicates(head *ListNode) *ListNode {
+
+	if head==nil ||head.Next==nil{
+		return head
+	}
+	if head.Val==head.Next.Val{
+		return deleteDuplicates(head.Next)
+	} else{
+		head.Next=deleteDuplicates(head.Next)
+		return head
+	}
+	
+}
