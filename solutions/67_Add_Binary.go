12e1fa8ba703d51075cb1ec49362af52d7165171
Mark <youngce0918@gmail.com>
2017-10-20 16:24:29 +0800

    optimize #67

--- solutions/67_Add_Binary.go
+++ solutions/67_Add_Binary.go
@@ -5,20 +5,23 @@ func getChar(s string,idx int) byte  {
 	return s[idx]
 }
 func addBinary(a string, b string) string {
-	if len(a)<len(b){
+	len_a:=len(a)
+	len_b:=len(b)
+	if len_a<len_b{
 		return addBinary(b,a)
 	}
 
-	diff:=len(a)-len(b)
-	res:=make([]byte,len(a)+1)
-	for i:=len(a)-1;i>=diff ;i--  {
+
+	diff:=len_a-len_b
+	res:=make([]byte,len_a+1)
+	for i:=len_a-1;i>=diff ;i--  {
 
 		res[i+1]= (a[i]+b[i-diff])-48
 
 	}
-	res[1:diff]=[]byte(a[0:diff])
+	
 
-	for i := len(a); i > 0; i-- {
+	for i := len_a; i > 0; i-- {
 
 		if i<=diff {
 			res[i]=res[i]+a[i-1]
